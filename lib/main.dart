83ae022946a6dad01902e61bfa99cd5142ba7246
youssef <youssefwagdy18@gmail.com>
2024-04-04 22:08:47 +0200

    empty project with empty home screen

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/home_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName :(context)=>HomeScreen(),
+      },
+    );
+  }
+}
